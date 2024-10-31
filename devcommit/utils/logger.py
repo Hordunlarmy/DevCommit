9321668b875ecaee82ea0f6c067fedf43f4beee9
Hordunlarmy <Hordunlarmy@gmail.com>
2024-10-31 13:31:07 +0100

    feat: Update to v0.1.3

--- devcommit/utils/logger.py
+++ devcommit/utils/logger.py
@@ -3,10 +3,17 @@ import os
 
 from decouple import Config, RepositoryEnv
 
-# Load configuration from the custom file
-home_dir = os.path.expanduser("~/.dcommit")
+if "VIRTUAL_ENV" in os.environ:
+    config_path = os.path.join(os.environ["VIRTUAL_ENV"], "config", ".dcommit")
+else:
+    config_path = os.path.expanduser("~/.dcommit")
 
-config = Config(RepositoryEnv(home_dir))
+if os.path.isfile(config_path):
+    config = Config(RepositoryEnv(config_path))
+    print("Configuration loaded from:", config_path)
+else:
+    config = None
+    print("No .dcommit configuration file found; proceeding with defaults.")
 
 
 class Logger:
@@ -15,7 +22,7 @@ class Logger:
     def __init__(
         self,
         logger_name: str,
-        log_file: str = "dc.logs",
+        log_file: str = "devcommits.logs",
         log_level: int = logging.DEBUG,
     ):
         self.logger = logging.getLogger(logger_name)
