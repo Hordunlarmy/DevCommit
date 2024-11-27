3ae6e324bff6b5f09058d287b3406c46c5e1bb30
Hordunlarmy <Hordunlarmy@gmail.com>
2024-11-27 12:37:17 +0100

    fix: Fix top_k unsupported value and update top_p and temperature

--- devcommit/app/gemini_ai.py
+++ devcommit/app/gemini_ai.py
@@ -32,9 +32,9 @@ def generateCommitMessage(diff: str) -> str:
         generation_config = {
             "response_mime_type": "text/plain",
             "max_output_tokens": 8192,
-            "top_k": 64,
-            "top_p": 0.95,
-            "temperature": 0.7,
+            "top_k": 20,
+            "top_p": 0.8,
+            "temperature": 0.5,
         }
 
         # Create Model and Start Chat
